1e4942c37c9e3e9ea1f72aabf8fa650ee98dc6bb
laura <meowmikyute.lemarchand@gmail.com>
2026-01-19 15:38:23 +0100

    html header footer

---
+++ htdocs/views/components/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <nav>
+        <ul class = "footer-titles">
+            <li> Outils</li>
+            <li> Ressources</li>
+            <li> Entreprises</li>
+            <li> Legal</li>
+        </ul></nav>
+</footer>
