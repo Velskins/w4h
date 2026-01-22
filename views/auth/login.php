53e34730ccae9955df37801434eaa4f713d3e029
Samir Mebarki <samir27893@gmail.com>
2026-01-22 16:44:27 +0100

    save

--- views/auth/login.php
+++ views/auth/login.php
@@ -2,8 +2,21 @@
     <?= htmlspecialchars($title) ?>
 </h2>
 
-<form action="/login" method="POST">
+<form>
+<main class="login">
+    <div class="login-content">
+        <h1>Connexion</h1>
 
+        <input type="text" placeholder="Pseudonyme">
+        <input type="password" placeholder="Mot de passe">
+
+        <a href="#" class="forgot">Mot de passe oublié ?</a>
 </form>
+        <button>Connexion</button>
 
-<p>Pas encore de compte ? <a href="/register">S'inscrire ici</a></p>
+        <p class="signup">
+            Vous n'êtes pas encore protégé ?
+            <a href="#">Inscrivez-vous !</a>
+        </p>
+    </div>
+</main>
